ee95c56a5b4b812ec4d7888e9b3ae1c8a65a6431
Petter Kvalvaag <pmkv@equinor.com>
2022-08-25 14:22:54 +0200

    Add model for posts

--- instapwn/web/models.py
+++ instapwn/web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    title = models.TextField()
+    content = models.TextField()
+
