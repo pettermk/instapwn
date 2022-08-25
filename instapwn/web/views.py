beed655b400bda96ed1a6cc69e1f8cb3491e6378
Petter Kvalvaag <pmkv@equinor.com>
2022-08-25 13:58:20 +0200

    Add user signin and registration

--- instapwn/web/views.py
+++ instapwn/web/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.contrib.auth import login
 
 
 def index(request):
     return render(request, 'web/index.html')
-    return HttpResponse("Hello, world. You're at the polls index.")
