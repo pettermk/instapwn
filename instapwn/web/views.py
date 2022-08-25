4eec3ffb820912a30887e976e1142708e7a7f8c1
Petter Kvalvaag <pmkv@equinor.com>
2022-08-25 21:58:46 +0200

    Handle creating posts

--- instapwn/web/views.py
+++ instapwn/web/views.py
@@ -5,6 +5,24 @@ from django.shortcuts import render
 from django.contrib.auth import login
 
 from web.forms import PostForm
+from web.models import Post
 
 def index(request):
-    return render(request, 'web/index.html', {'PostForm': PostForm()})
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = PostForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            post = Post(
+                author = request.user,
+                title=form.cleaned_data['title'], content=form.cleaned_data['content'])
+            post.save()
+    posts = Post.objects.all()
+    return render(
+        request,
+        'web/index.html',
+        {
+            'PostForm': PostForm(),
+            'posts': posts
+        }
+    )
