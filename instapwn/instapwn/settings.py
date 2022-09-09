22236f2fd86f8da0c2633faa277b391cf9defcf3
Petter Kvalvaag <pmkv@equinor.com>
2022-09-09 15:27:09 +0200

    Disable CSP from starting point

--- instapwn/instapwn/settings.py
+++ instapwn/instapwn/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'csp.middleware.CSPMiddleware',
+#     'csp.middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'instapwn.urls'
